67fe407421b54036e749ed563c03e521efdd056e
Samuel Lackey <34922533+slackey14@users.noreply.github.com>
2023-11-11 14:50:29 -0600

    text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -33,9 +33,9 @@ function Home() {
     
             <Card style={{ width: '18rem', marginRight: 'calc(0px + 2.5vw)', backgroundColor: '#eae6deff', borderRadius: '10px' }}>
               <Card.Body>
-              <Card.Title>Data</Card.Title>
+              <Card.Title>Analysis</Card.Title>
                 <Card.Text>
-                Understanding the diverse backgrounds of our students is crucial for providing personalized and effective education. Although a range of personal information is collected,   only the necessary information is shown to the professor. It's important to note that this information is treated with the utmost confidentiality, and only the essential details required for customized support are shared with educators. Our commitment to privacy ensures a secure and nurturing learning environment for every student.
+                In leveraging the collected data, our educational platform conducts insightful analyses to tailor learning experiences for individual students. By examining a range of student paramteres, we identify patterns and trends that help us better understand the unique needs of each learner. This data-driven approach enables us to implement targeted strategies, ensuring that educational resources and support are aligned with the diverse backgrounds of our students. Through continuous analysis, we strive to enhance engagement, foster inclusivity, and promote academic success for every individual in our learning community.
                 </Card.Text>
               </Card.Body>
             </Card>
