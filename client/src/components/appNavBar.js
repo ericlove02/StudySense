90f50589e768c21a94bcfffa275ac0fd109cede1
Jameson Adams <jamdadam@gmail.com>
2023-11-11 14:33:12 -0600

    Student and nav

--- client/src/components/appNavBar.js
+++ client/src/components/appNavBar.js
@@ -10,10 +10,10 @@ function NavBar() {
     return (
         <Navbar bg="light" style={{ backgroundColor: '#500000ff', color: 'white', zIndex: 1000 }}>
         <Container>
-          <Navbar.Brand href="#home" style={{ color: 'white', textDecoration: 'none'}}>Garbonzo</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{ color: 'white', textDecoration: 'none', fontSize: '45px'}}>Garbonzo</Navbar.Brand>
           <Nav className="ml-auto">
-          <Nav.Link href="#home" style={{ color: 'white', marginRight: '15px', textDecoration: 'none'}}>Home</Nav.Link>
-          <Nav.Link href="#login" style={{ color: 'white', textDecoration: 'none'}}>Login</Nav.Link>
+          <Nav.Link href="#home" style={{ color: 'white', marginRight: '15px', textDecoration: 'none', fontSize: '25px'}}>Home</Nav.Link>
+          <Nav.Link href="#login" style={{ color: 'white', textDecoration: 'none',fontSize: '25px' }}>Login</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
