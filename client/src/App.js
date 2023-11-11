5c2ae3693e19e53a87ccb4a022b9a93049c7c355
ericlove02 <eric.love02@yahoo.com>
2023-11-11 11:11:57 -0600

    created pages sort of

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Student from './pages/Student';
+import Teacher from './pages/Teacher';
 
 function App() {
-  const [time, setTime] = useState([{}])
-
-  useEffect(() => {
-    fetch("/time").then(
-      res => res.json()
-        .then(
-          time => {
-            setTime(time)
-            console.log(time)
-          }
-        )
-    )
-  }, [])
 
   return (
-    <div className="App">
-      {time.time}
-    </div>
+    <div><Teacher /></div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Navigate replace to="/home" />} />
+    //     {/* <Route path="/" render={() => {
+    //       return (
+    //         // check if user is auth, if yes send home, else send to login
+    //         this.isAuth ?
+    //         <Navigate to="/home" /> :
+    //         <Navigate
+    //       ) }} />*/}
+    //     <Route path="/home" element={<Home />} />
+    //     <Route path="/student" element={<Student />} />
+    //     <Route path="/teacher" element={<Teacher />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
